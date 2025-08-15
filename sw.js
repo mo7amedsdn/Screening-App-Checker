c1d8079f1461938c633b12cb6cfb4c76b2fe96ab
mo7amedsdn <mo7amedsdn@gmail.com>
2025-08-15 15:40:11 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install',function(e){console.log('Service Worker installed')});
+self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+
